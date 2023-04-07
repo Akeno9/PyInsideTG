af33abd17c1a10980e11fde28f8d76103293713a
Akeno9 <125627906+Akeno9@users.noreply.github.com>
2023-04-07 21:54:32 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-   return 'Hello, World!'
-
-os.system("pip3 install -U -r requirements.txt && python3 main.py")
